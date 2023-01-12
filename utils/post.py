58fbb3ce804b17ee0913ebd01f2891e61288cd66
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2023-01-12 20:28:50 +1100

    added first post

---
+++ utils/post.py
@@ -0,0 +1,55 @@
+from time import gmtime, strftime, struct_time, time
+from json import load
+
+from typing import Self
+
+
+template = """
+            <article>
+                <h2><a href="/{path}">{title}</a></h2>
+                <hr>
+                <time class="meta" datetime="{fdate}">{date}</time>{sep}<span class="meta location">{loc}</span></data>
+                <br>
+                {content}
+            </article>
+""".rstrip()
+
+
+class Post:
+    title: str
+    path: str
+    date: struct_time
+    loc: str
+    content: str
+
+    @classmethod
+    def fromJSON(cls, path: str) -> Self:
+        p = cls()
+
+        with open(path, "r") as f:
+            data = load(f)
+
+        p.title = data["title"]
+        p.path = data["path"]
+        p.date = gmtime(data["date"])
+        p.loc = data["loc"]
+        p.content = data["content"]
+
+        return p
+
+    def toHTML(self) -> str:
+        return template.format(
+            path=self.path,
+            title=self.title,
+            fdate=strftime(r"%Y-%m-%d %H:%M:%S", self.date),
+            date=strftime(r"%I:%M %p, %Y-%m-%d", self.date),
+            sep=", " * bool(self.loc),
+            loc=self.loc,
+            content=self.content.replace("\n", "\n                ")
+        )
+
+if __name__ == "__main__":
+    now = gmtime()
+
+    with open("posts/%d-%s-.json" % (now.tm_year, str(now.tm_mon).zfill(2)), "w") as f:
+        f.write("{\n    \"title\": \"\",\n    \"path\": \"posts/%d-%s-000.html\",\n    \"date\": %d,\n    \"loc\": \"\",\n    \"content\": \"\"\n}" % (now.tm_year, str(now.tm_mon).zfill(2), int(time())))
---
+++ utils/update.py
@@ -0,0 +1,54 @@
+from time import gmtime
+from os import walk
+from re import sub
+
+from post import Post
+
+
+now = gmtime()
+
+template = """
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>{page} | GoodCoderBBoy's Blog</title>
+		<link rel="stylesheet" href="/css/style.css">
+		<link rel="icon" href="/media/Avatar.jpeg" />
+	</head>
+	<body>
+		<header>
+			<h1 id="title"><a href="/">GoodCoderBBoy</a></h1>
+			<img id="icon" src="/media/Avatar.jpeg">
+            <nav></nav>
+		</header>
+        <aside></aside>
+        <main>{content}
+        </main>
+		<footer>
+			<span id="copyright">&copy; %d GoodCoderBBoy</span>
+		</footer>
+	</body>
+</html>
+""".strip() % now.tm_year
+
+posts: list[Post] = []
+for path, dirs, files in walk("posts"):
+    for file in files:
+        if file.endswith(".json"):
+            posts.append(Post.fromJSON(f"posts/{file}"))
+
+
+if __name__ == "__main__":
+    home = []
+
+    for p in posts:
+        content = p.toHTML()
+
+        home.append(content)
+
+        with open(f"{p.path}", "w") as f:
+            f.write(template.format(page=p.title, content=content))
+
+    with open("index.html", "w") as f:
+        f.write(template.format(page="Home", content="".join(home)))
